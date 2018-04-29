bbfa10fc71de0fa974166e7e85723d438022e037
Syed Haider <shayder426@gmail.com>
2018-04-28 22:21:20 -0700

    testing new branch

--- Tut4YouWebApp/src/main/java/tut4you/controller/AvailabilityBean.java
+++ Tut4YouWebApp/src/main/java/tut4you/controller/AvailabilityBean.java
@@ -43,7 +43,7 @@ import tut4you.model.Tut4YouApp;
  * modified by Syed Haider <shayder426@gmail.com>
  */
 @Named
-@ViewScoped
+@SessionScoped
 public class AvailabilityBean implements Serializable {
 
     private static final long serialVersionUID = 1L;

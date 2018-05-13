aa82653b6a0ee85be1427e81ad6e2bd229c62eb5
akaichi <ahkaichi@users.noreply.github.com>
2018-05-12 17:59:49 -0700

    created Question and Reponse entity classes

---
+++ Tut4YouWebApp/src/main/java/tut4you/model/Question.java
@@ -0,0 +1,122 @@
+/*
+ * Licensed under the Academic Free License (AFL 3.0).
+ *     http://opensource.org/licenses/AFL-3.0
+ * 
+ *  This code has been developed by a group of CSULB students working on their 
+ *  Computer Science senior project called Tutors4You.
+ *  
+ *  Tutors4You is a web application that students can utilize to find a tutor and
+ *  ask them to meet at any location of their choosing. Students that struggle to understand 
+ *  the courses they are taking would benefit from this peer to peer tutoring service.
+ 
+ *  2017 Amanda Pan <daikiraidemodaisuki@gmail.com>
+ *  2017 Andrew Kaichi <ahkaichi@gmail.com>
+ *  2017 Keith Tran <keithtran25@gmail.com>
+ *  2017 Syed Haider <shayder426@gmail.com>
+ */
+package tut4you.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author Andrew Kaichi <ahkaichi@gmail.com>
+ */
+@Entity
+public class Question implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String description;
+    
+    @OneToMany
+    private Response response;
+    
+    @OneToOne
+    private Course course;
+
+    public Question() {
+    
+    }
+    
+    public Question(String title, String description, Response response) {
+        this.title = title;
+        this.description = description;
+        this.response = response;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description){
+        this.description = description;
+    }
+    
+    public Response getResponse(){
+        return response;
+    }
+    
+    public void setResponse(Response response){
+        this.response = response;
+    }
+    
+    public Course getCourse(){
+        return course;
+    }
+    
+    public void setCourse(Course course){
+        this.course = course;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Question)) {
+            return false;
+        }
+        Question other = (Question) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "tut4you.model.Question[ id=" + id + " ]";
+    }
+    
+}
---
+++ Tut4YouWebApp/src/main/java/tut4you/model/Response.java
@@ -0,0 +1,119 @@
+/*
+ * Licensed under the Academic Free License (AFL 3.0).
+ *     http://opensource.org/licenses/AFL-3.0
+ * 
+ *  This code has been developed by a group of CSULB students working on their 
+ *  Computer Science senior project called Tutors4You.
+ *  
+ *  Tutors4You is a web application that students can utilize to find a tutor and
+ *  ask them to meet at any location of their choosing. Students that struggle to understand 
+ *  the courses they are taking would benefit from this peer to peer tutoring service.
+ 
+ *  2017 Amanda Pan <daikiraidemodaisuki@gmail.com>
+ *  2017 Andrew Kaichi <ahkaichi@gmail.com>
+ *  2017 Keith Tran <keithtran25@gmail.com>
+ *  2017 Syed Haider <shayder426@gmail.com>
+ */
+package tut4you.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author Andrew Kaichi <ahkaichi@gmail.com>
+ */
+@Entity
+public class Response implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String explanation;
+    private String answer;
+    @OneToOne
+    private Question question;
+    @OneToOne
+    private Tutor tutor;
+
+    public Response() {
+    
+    }
+    
+    public Response(String explanation, String answer, Tutor tutor) {
+        this.explanation = explanation;
+        this.answer = answer;
+        this.tutor = tutor;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getExplanation() {
+        return explanation;
+    }
+    
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+    
+    public String getAnswer() {
+        return answer;
+    }
+    
+    public void setAnswer(String answer){
+        this.answer = answer;
+    }
+    
+    public Tutor getTutor(){
+        return tutor;
+    }
+    
+    public void setTutor(Tutor tutor){
+        this.tutor = tutor;
+    }
+    
+    public Question getQuestion(){
+        return question;
+    }
+    
+    public void setQuestion(Question question){
+        this.question = question;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Response)) {
+            return false;
+        }
+        Response other = (Response) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "tut4you.model.Response[ id=" + id + " ]";
+    }
+    
+}
